9855197fba225d8eff7f6313a1de41d448e65825
Hiki <aneureka2@gmail.com>
2018-06-15 14:59:11 +0800

    add readme

--- app/api/index.py
+++ app/api/index.py
@@ -6,5 +6,19 @@ from app import db
 
 @api.route('/', methods=['GET'])
 def index():
-    return '连接成功~'
+    return '已经连接到服务器啦~'
+
+
+@api.route('/api')
+def show_api():
+    html = """
+           <h2>internship_fetcher api list</h2>
+           <h3>V2EX</h3>
+           <ul>
+           <li>/api/v2ex</li>
+           <li>/api/v2ex/today</li>
+           <li>/api/v2ex/yesterday</li>
+           </ul>
+           """
+    return html
 
